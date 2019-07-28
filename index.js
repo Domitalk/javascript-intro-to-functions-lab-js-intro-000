ef0fc3a502d6d3c31eb9edb97f98d617e28ff0b1
Dominic <dominic.m.chu@gmail.com>
2019-07-28 04:09:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (var lowercase = 'hello!'
-  lowercase.toLowerCase() === lowercase) {
+  if (string.toLowerCase() === string) {
   return "I can\'t hear you!"}
 }
